b5614bd6606bd9a6240d14d21a8309fac7699d34
Anticlub <cristofdzfdz@gmail.com>
2026-03-25 13:27:37 +0100

    feat: implement m3u parser with coroutines and safe index access

---
+++ app/src/main/java/com/example/videoplayer_kt/data/local/M3uParser.kt
@@ -0,0 +1,39 @@
+package com.example.videoplayer_kt.data.local
+
+import android.util.Log
+import com.example.videoplayer_kt.domain.models.Channel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class M3uParser {
+    suspend fun parse(content: String): List<Channel> = withContext(Dispatchers.IO){
+        val lines = content.lines()
+        val channels = mutableListOf<Channel>()
+
+        lines.forEachIndexed { index, line ->
+            if (line.startsWith("#EXTINF")){
+                // TODO: eliminar log al final
+                Log.d("M3uParser",line)
+                val nextLine = lines.getOrNull(index + 1)
+                if (nextLine.isNullOrBlank()) { return@forEachIndexed }
+                if(nextLine.startsWith("#")){ return@forEachIndexed }
+                val url = nextLine
+                val name = line.substringAfterLast(",")
+                val tvgId = extractAttribute(line, "tvg-id")
+                val tvgLogo = extractAttribute(line, "tvg-logo")
+                val groupTitle = extractAttribute(line, "group-title")
+
+                val channel = Channel(0, tvgId, name, url, groupTitle, tvgLogo)
+
+                channels.add(channel)
+
+            }
+        }
+        channels
+    }
+
+    private fun extractAttribute(line: String, attribute: String): String? {
+        val regex = Regex("""$attribute="([^"]*)""")
+        return regex.find(line)?.groupValues?.get(1)
+    }
+}
