7e85d678cf264d20ff6b24954132e36bd85599aa
Anticlub <cristofdzfdz@gmail.com>
2026-03-26 13:50:52 +0100

    feat: add ChannelRepositoryImpl and PlaylistRepositoryImpl

--- app/src/main/java/com/example/videoplayer_kt/domain/repository/ChannelRepository.kt
+++ app/src/main/java/com/example/videoplayer_kt/domain/repository/ChannelRepository.kt
@@ -5,11 +5,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface ChannelRepository {
 
-    fun getAllChannels(): Flow<List<Channel>>
-    fun getChannelsByGroup(group: String): Flow<List<Channel>>
-    fun getFavoriteChannels(): Flow<List<Channel>>
-    suspend fun addToFavorites(channel: Channel)
-    suspend fun removeFromFavorites(channel: Channel)
-    suspend fun saveChannels(channel: List<Channel>)
-    suspend fun clearAllChannels()
+    fun getChannelsByPlaylist(playlistId: Long): Flow<List<Channel>>
+    suspend fun clearAllChannels(playlistId: Long)
+    suspend fun saveChannels(channels: List<Channel>, playlistId: Long)
+    // TODO: fase 5
+    //  fun getChannelsByGroup(group: String): Flow<List<Channel>>
+    // TODO: fase 7 - favoritos
+    //  suspend fun addToFavorites(channels: Channel)
+    //  suspend fun removeFromFavorites(channel: Channel)
+    //  fun getFavoriteChannels(): Flow<List<Channel>>
 }
