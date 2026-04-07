b5ef0ffc61747fa0490f1235b0a3067d307d42a2
Anticlub <cristofdzfdz@gmail.com>
2026-04-07 12:15:46 +0200

    feat: add dialog layout and setup PlaylistFragment with RecyclerView and FAB

--- app/src/main/java/com/example/videoplayer_kt/presentation/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/example/videoplayer_kt/presentation/playlist/PlaylistFragment.kt
@@ -5,14 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.videoplayer_kt.databinding.FragmentPlaylistBinding
 
 class PlaylistFragment: Fragment() {
+
+    private var _binding: FragmentPlaylistBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PlaylistViewModel by viewModels()
+    private val adapter = PlaylistAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupFab()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvPlaylist.adapter = adapter
+        binding.rvPlaylist.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun setupFab() {
+        binding.fabAddPlaylist.setOnClickListener {
+            // aquí abriremos el dialog
+            showAddPlaylistDialog()
+        }
     }
 }
