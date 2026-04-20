38d6708a5703b9e1a3fc221f91f525a9607810ae
Anticlub <cristofdzfdz@gmail.com>
2026-04-20 16:42:22 +0200

    feat: add PlaylistRemoteDataSource interface

--- app/src/main/java/com/example/videoplayer_kt/data/remote/datasource/PlaylistRemoteDataSource.kt
+++ app/src/main/java/com/example/videoplayer_kt/data/remote/datasource/PlaylistRemoteDataSource.kt
@@ -1,7 +1,10 @@
 package com.example.videoplayer_kt.data.remote.datasource
 
+import com.example.videoplayer_kt.data.remote.dto.PlaylistDto
 import com.example.videoplayer_kt.domain.models.Playlist
 
 interface PlaylistRemoteDataSource {
-    suspend fun syncPlaylists(playlists: List<Playlist>): Unit
+    suspend fun syncPlaylists(playlists: List<PlaylistDto>)
+    suspend fun uploadPlaylist(playlist: PlaylistDto)
+    suspend fun deletePlaylist(playlistId: Long)
 }
