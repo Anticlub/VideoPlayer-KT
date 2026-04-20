37e36d847e9eff01a1b6ba4b33afe5b4b25dd8be
Anticlub <cristofdzfdz@gmail.com>
2026-04-20 10:17:04 +0200

    refactor: use cases return domain models instead of DataResult

--- app/src/main/java/com/example/videoplayer_kt/domain/usecases/auth/LogoutUseCase.kt
+++ app/src/main/java/com/example/videoplayer_kt/domain/usecases/auth/LogoutUseCase.kt
@@ -1,14 +1,12 @@
 package com.example.videoplayer_kt.domain.usecases.auth
 
-import com.example.videoplayer_kt.domain.models.DataResult
 import com.example.videoplayer_kt.domain.repository.AuthRepository
-
 import javax.inject.Inject
 
 class LogoutUseCase @Inject constructor(
     private val repository: AuthRepository
 ) {
-    suspend operator fun invoke() : DataResult<Unit> {
+    suspend operator fun invoke() {
         return repository.logout()
     }
 }
