34172cd3ce8b8632736fd7606f1ad4b5bef758dd
Anticlub <cristofdzfdz@gmail.com>
2026-04-21 10:07:41 +0200

    feat: add SyncPlaylistUseCase and UploadPlaylistUseCase

---
+++ app/src/main/java/com/example/videoplayer_kt/domain/usecases/playlist/UploadPlaylistsUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.videoplayer_kt.domain.usecases.playlist
+
+import com.example.videoplayer_kt.domain.models.Playlist
+import com.example.videoplayer_kt.domain.repository.PlaylistRepository
+import javax.inject.Inject
+
+class UploadPlaylistsUseCase @Inject constructor(
+    private val repository: PlaylistRepository
+) {
+    suspend operator fun invoke(playlist: Playlist) {
+        return repository.uploadPlaylist(playlist)
+    }
+}
