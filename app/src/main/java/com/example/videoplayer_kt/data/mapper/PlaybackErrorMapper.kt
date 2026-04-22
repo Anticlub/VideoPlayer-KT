1e0f887387e934daa4273d6c5c2a345c22c6ff51
Anticlub <cristofdzfdz@gmail.com>
2026-04-22 10:27:20 +0200

    feat(player): improve playback error handling with typed errors

---
+++ app/src/main/java/com/example/videoplayer_kt/data/mapper/PlaybackErrorMapper.kt
@@ -0,0 +1,21 @@
+package com.example.videoplayer_kt.data.mapper
+
+import androidx.media3.common.PlaybackException
+import com.example.videoplayer_kt.domain.models.PlaybackError
+
+object PlaybackErrorMapper {
+    fun map(error: PlaybackException) : PlaybackError {
+        return when (error.errorCode) {
+            PlaybackException.ERROR_CODE_DRM_SYSTEM_ERROR,
+            PlaybackException.ERROR_CODE_DRM_LICENSE_EXPIRED,
+            PlaybackException.ERROR_CODE_DRM_CONTENT_ERROR -> PlaybackError.Drm
+            PlaybackException.ERROR_CODE_IO_NETWORK_CONNECTION_FAILED,
+            PlaybackException.ERROR_CODE_IO_NETWORK_CONNECTION_TIMEOUT -> PlaybackError.Network
+            PlaybackException.ERROR_CODE_IO_BAD_HTTP_STATUS,
+            PlaybackException.ERROR_CODE_PARSING_CONTAINER_MALFORMED,
+            PlaybackException.ERROR_CODE_PARSING_MANIFEST_MALFORMED -> PlaybackError.Source
+            else -> PlaybackError.Unknown(error.localizedMessage)
+
+        }
+    }
+}
