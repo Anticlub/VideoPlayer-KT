5098198da73c26ec8161dfd2392b095272bbc0ef
Anticlub <cristofdzfdz@gmail.com>
2026-04-27 10:14:07 +0200

    fix: add @Inject constructor to DetectUrlTypeUseCase for Hilt

--- app/src/main/java/com/example/videoplayer_kt/domain/usecases/playlist/DetectUrlTypeUseCase.kt
+++ app/src/main/java/com/example/videoplayer_kt/domain/usecases/playlist/DetectUrlTypeUseCase.kt
@@ -3,8 +3,9 @@ package com.example.videoplayer_kt.domain.usecases.playlist
 import com.example.videoplayer_kt.domain.models.Channel
 import com.example.videoplayer_kt.domain.util.StreamUrlToChannelMapper
 import com.example.videoplayer_kt.domain.util.StreamValidator
+import javax.inject.Inject
 
-class DetectUrlTypeUseCase {
+class DetectUrlTypeUseCase @Inject constructor() {
     operator fun invoke(url: String): Channel? {
         return if (StreamValidator.isDirectStreamUrl(url)) StreamUrlToChannelMapper.map(url) else null
     }
